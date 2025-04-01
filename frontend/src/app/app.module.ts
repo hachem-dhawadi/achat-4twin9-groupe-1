bb1c92aca2f5ee70af13bc1e9d156ea562185bde
Mokhles Benna <mokhles.elbenna@esprit.tn>
2025-04-01 11:44:43 +0200

    🪵 Added frontend and backend logging (ngx-logger + SLF4J)

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,19 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { ProductsComponent } from './products/products.component';
 import { FormsModule } from '@angular/forms';
-import { NgbModal, NgbModalModule, NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgbModalModule, NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { RouterModule } from '@angular/router';
-import { NgModule } from '@angular/core';
+
 import { StockComponent } from './stock/stock.component';
 import { ReglementComponent } from './reglement/reglement.component';
 import { SecteurActiviteComponent } from './secteur-activite/secteur-activite.component';
 import { OperateurComponent } from './operateur/operateur.component';
 import { FactureComponent } from './facture/facture.component';
 import { NavbarComponent } from './navbar/navbar.component';
+
+// ✅ Logging module
+import { LoggerModule, NGXLogger, NgxLoggerLevel } from 'ngx-logger';
 
 @NgModule({
   declarations: [
@@ -33,9 +37,16 @@ import { NavbarComponent } from './navbar/navbar.component';
     FormsModule,
     NgbModalModule,
     RouterModule,
-    NgbModule
+    NgbModule,
+
+    // ✅ Logging configuration
+    LoggerModule.forRoot({
+      level: NgxLoggerLevel.DEBUG,
+      serverLogLevel: NgxLoggerLevel.OFF,
+      serverLoggingUrl: ''
+    })
   ],
-  providers: [],
+  providers: [NGXLogger],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
